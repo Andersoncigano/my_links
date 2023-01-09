59895538a0ebeefc8dd341280abd23e0f4ff3e35
Andersoncigano <slavedreamers@gmail.com>
2023-01-09 18:53:02 -0300

    teste initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function togglemode() {
+
+const html = document.documentElement
+
+//if(html.classList.contains (light)) {
+   // html.classList.remove('light')
+
+//} else {
+   
+
+html.classList.toggle("light")
+
+
+}
